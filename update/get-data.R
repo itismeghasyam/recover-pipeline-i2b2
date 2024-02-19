b46b97c7a4749841fc3b504a76f3d4537ec50906
Pranav Anbarasu <pranavanba@gmail.com>
2024-02-19 19:22:37 +0000

    Only sync bucket instead of opening all datasets in this step; create temp dir

--- update/get-data.R
+++ update/get-data.R
@@ -10,7 +10,9 @@ config::get(
 
 synLogin()
 
-concept_map <- syn_file_to_df(ontologyFileID, "concept_cd")
+concept_map <- 
+  syn_file_to_df(ontologyFileID, "CONCEPT_CD") %>% 
+  filter(CONCEPT_CD!="<null>")
 
 selected_vars <- 
   syn_file_to_df(selectedVarsFileID) %>% 
@@ -18,28 +20,32 @@ selected_vars <-
 
 dataset_name_filter <- selected_vars %>% dplyr::pull(Export) %>% unique()
 
-df_list_original <- 
-  syn_parquet_dataset_to_dflist(
-    parquetDirID, 
-    method, 
-    s3bucket, 
-    s3basekey, 
-    downloadLocation, 
-    dataset_name_filter,
-    deleteExistingDir = FALSE
-  )
+# Sync S3 bucket to local
+token <- synapser::synGetStsStorageToken(
+  entity = parquetDirID,
+  permission = "read_only",
+  output_format = "json")
 
-df_list_unified_tmp <- 
-  unify_dfs(df_list_original) %>% 
-  lapply(function(x) {
-    names(x) <- tolower(names(x))
-    return(x)})
+if (s3bucket==token$bucket && s3basekey==token$baseKey) {
+  base_s3_uri <- paste0('s3://', token$bucket, '/', token$baseKey)
+} else {
+  base_s3_uri <- paste0('s3://', s3bucket, '/', s3basekey)
+}
 
-df_list <- 
-  df_list_unified_tmp %>% 
-  lapply(function(df) {
-    names(df) <- gsub("value", "value_original", names(df))
-    return(df)
-  })
+Sys.setenv('AWS_ACCESS_KEY_ID'=token$accessKeyId,
+           'AWS_SECRET_ACCESS_KEY'=token$secretAccessKey,
+           'AWS_SESSION_TOKEN'=token$sessionToken)
+
+if (deleteExistingDir==TRUE) {
+  unlink(downloadLocation, recursive = T, force = T)
+}
+
+inclusions <- paste0("--include \"*",dataset_name_filter,"*\"", collapse = " ")
+sync_cmd <- glue::glue('aws s3 sync {base_s3_uri} {downloadLocation} --exclude "*" {inclusions}')
+system(sync_cmd)
 
 concept_replacements_reversed <- vec_reverse(concept_replacements)
+
+if (!dir.exists(outputConceptsDir)) {
+  dir.create(outputConceptsDir)
+}
