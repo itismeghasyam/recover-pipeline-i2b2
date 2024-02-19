3b68c3ef496c43cc7b74ae376f654a35f38a867a
Pranav Anbarasu <pranavanba@gmail.com>
2024-02-19 19:21:46 +0000

    Transform fitbitactivitylogs data and output i2b2 summaries for selected vars from this dataset

--- i2b2measures/fitbitactivitylogs.R
+++ i2b2measures/fitbitactivitylogs.R
@@ -7,7 +7,45 @@ vars <-
   tolower()
 
 df <- 
-  df_list$fitbitactivitylogs %>%
-  select(all_of(vars))
+  arrow::open_dataset(file.path(downloadLocation, "dataset_fitbitactivitylogs")) %>% 
+  select(matches(vars, ignore.case = TRUE)) %>% 
+  collect()
 
-excluded_concepts <- diff_concepts(df_list, concept_replacements, concept_map, concept_filter_col)
+colnames(df) <- tolower(colnames(df))
+
+excluded_concepts <- c("participantidentifier", "startdate", "enddate")
+
+approved_concepts_summarized <- 
+  setdiff(
+    tolower(selected_vars$Variable[selected_vars$Export=="fitbitactivitylogs"]),
+    excluded_concepts
+  )
+
+df_melted_filtered <- 
+  df %>% 
+  recoverSummarizeR::melt_df(excluded_concepts = excluded_concepts) %>% 
+  select(if("participantidentifier" %in% colnames(.)) "participantidentifier",
+         dplyr::matches("(?<!_)date(?!_)", perl = T),
+         if("concept" %in% colnames(.)) "concept",
+         if("value" %in% colnames(.)) "value") %>% 
+  tidyr::drop_na("value") %>% 
+  mutate(value = as.numeric(value))
+cat("recoverSummarizeR::melt_df() completed.\n")
+
+df_summarized <- 
+  df_melted_filtered %>% 
+  select(all_of(c("participantidentifier", "startdate", "enddate", "concept", "value"))) %>% 
+  recoverSummarizeR::stat_summarize() %>% 
+  distinct()
+cat("recoverSummarizeR::stat_summarize() completed.\n")
+
+output_concepts <- 
+  process_df(df_summarized, concept_map, concept_replacements_reversed, concept_map_concepts = "CONCEPT_CD", concept_map_units = "UNITS_CD") %>% 
+  dplyr::mutate(nval_num = signif(nval_num, 9)) %>% 
+  dplyr::arrange(concept) %>% 
+  dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character)) %>% 
+  replace(is.na(.), "<null>") %>% 
+  dplyr::filter(nval_num != "<null>" | tval_char != "<null>")
+
+output_concepts %>% 
+  write.csv(file.path(outputConceptsDir, "fitbitactivitylogs.csv"), row.names = F)
