e8d5a9dccc260990bae850a423b5cf10d100e3fe
Pranav Anbarasu <pranavanba@gmail.com>
2024-02-20 18:04:17 +0000

    Get list of participants with fitbit devices

---
+++ scripts/process_data/fitbitdevices.R
@@ -0,0 +1,22 @@
+library(dplyr)
+
+vars <- 
+  selected_vars %>% 
+  filter(grepl("fitbitdevices", Export, ignore.case = TRUE)) %>% 
+  pull(Variable)
+
+df <- 
+  arrow::open_dataset(file.path(downloadLocation, "dataset_fitbitdevices")) %>% 
+  select(all_of(vars)) %>% 
+  collect()
+
+colnames(df) <- tolower(colnames(df))
+
+participant_devices <- 
+  df %>% 
+  distinct(participantidentifier) %>% 
+  mutate(device = "fitbit")
+
+participant_devices %>% 
+  write.csv(file.path(outputConceptsDir, "participant_devices.csv"), row.names = F)
+cat(glue::glue("participant_devices written to {file.path(outputConceptsDir, 'participant_devices.csv')}\n"))
